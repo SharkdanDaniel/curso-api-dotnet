ae59f15e63d2fc6733306d2c944798ed82c57fcf
Daniel <sharkdan_daniel@hotmail.com>
2021-05-07 09:38:07 -0300

    criado o application, data, crosscutting e domain

---
+++ API.Data/Context/ContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=localhost; port=3306; Database=dbAPI; Uid=root; Pwd=mysql";
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 23));
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ API.Data/Context/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using API.Data.Mapping;
+using API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base (options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+        }
+    }
+}
